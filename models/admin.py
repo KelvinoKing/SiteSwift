885e15dcd27baa3e76b8bd5d768596d37eb5845b
KelvinoKing <kelvinoking@outlook.com>
2024-05-23 16:31:34 +0300

    Added admin page

---
+++ models/admin.py
@@ -0,0 +1,16 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+
+
+class Admin(BaseModel, Base):
+    """This class will define the admin model for SITESWIFT"""
+
+    __tablename__ = 'admin'
+    first_name = Column(String(80), nullable=False)
+    last_name = Column(String(80), nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password_hash = Column(String(128), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
